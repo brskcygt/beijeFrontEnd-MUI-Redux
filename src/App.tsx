a160c02235e962c671e649c474e45c9709bea4bf
brkcygt <bariskoc.249@gmail.com>
2023-05-24 16:23:52 +0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RootComponent from './RootComponent'
+// import { persistor, store } from './store/reducers/store'
+
+
+const App: React.FC = () => {
+    return <RootComponent />
+}
+
+export default App
